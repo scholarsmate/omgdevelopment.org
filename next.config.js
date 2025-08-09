26e5191bea77444af7b85c70132ac3d413e66191
Davin Shearer <2205472+scholarsmate@users.noreply.github.com>
2025-08-09 00:01:36 -0400

    chore: enable MDX via @next/mdx with GFM; update deps

--- next.config.js
+++ next.config.js
@@ -1,19 +1,11 @@
-module.exports = {
+const remarkGfm = require('remark-gfm');
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkGfm],
+  },
+});
+
+module.exports = withMDX({
   pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
-  webpack(config, options) {
-    const remarkGfm = require('remark-gfm');
-    config.module.rules.push({
-      test: /\.mdx?$/,
-      use: [
-        options.defaultLoaders.babel,
-        {
-          loader: '@mdx-js/loader',
-          options: {
-            remarkPlugins: [remarkGfm],
-          },
-        },
-      ]
-    });
-    return config;
-  }
-};
+});
